2fec47c3e5c1d76b3cbe37cff8cd40a12c4014b2
rafaelgssa <rafael.gssa@gmail.com>
2020-10-27 15:07:41 -0300

    Move sync options to sidebar

--- src/modules/history/components/HistoryActions.tsx
+++ src/modules/history/components/HistoryActions.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Divider } from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
 import * as React from 'react';
 import { I18N } from '../../../common/I18N';
 
@@ -9,8 +10,10 @@ interface HistoryActionsProps {
 
 export const HistoryActions: React.FC<HistoryActionsProps> = (props: HistoryActionsProps) => {
 	const { onNextPageClick, onSyncClick } = props;
+	const theme = useTheme();
+
 	return (
-		<Box className="history-actions--container">
+		<Box className="history-actions--container" style={{ zIndex: theme.zIndex.appBar }}>
 			<Divider />
 			<Box className="history-actions">
 				<Button onClick={onNextPageClick} variant="contained">
