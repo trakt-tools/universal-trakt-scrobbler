c665b3741b2d2804c4efefeddbb7f8f66f4b2862
rafaelgomesxyz <rafaelgomesxyz@gmail.com>
2021-07-14 20:59:53 -0300

    Enclose lint-staged file names with double quotes

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,9 @@
 module.exports = {
 	'*.{json,css,html,md,yml,yaml}': 'prettier --write',
-	'*.{js,jsx,ts,tsx}': (filenames) => [
+	'*.{js,jsx,ts,tsx}': (fileNames) => [
 		'tsc --noEmit -p ./tsconfig.json',
-		`eslint --fix --quiet -c ./.eslintrc.typed.js --no-eslintrc ${filenames.join(' ')}`,
+		`eslint --fix --quiet -c ./.eslintrc.typed.js --no-eslintrc ${fileNames
+			.map((fileName) => `"${fileName}"`)
+			.join(' ')}`,
 	],
 };
