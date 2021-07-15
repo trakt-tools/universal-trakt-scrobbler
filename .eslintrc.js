ab24185867357ed5cb1c0fcfdd032ce8611b73f7
rafaelgomesxyz <rafaelgomesxyz@gmail.com>
2021-07-14 21:06:33 -0300

    Use cross-env to detect whether or not to run typed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,86 @@
 const path = require('path');
 
+const ROOT_DIR = __dirname;
+const SRC_DIR = path.resolve(ROOT_DIR, 'src');
+
+const getTranslationsOverride = () => {
+	return {
+		files: ['**/_locales/**/*.json'],
+		plugins: ['@rafaelgomesxyz/i18n-json'],
+		extends: ['plugin:@rafaelgomesxyz/i18n-json/recommended'],
+		rules: {
+			'@rafaelgomesxyz/i18n-json/identical-keys': [
+				'error',
+				{
+					filePath: path.resolve(SRC_DIR, '_locales', 'en', 'messages.json'),
+					checkDuplicateValues: true,
+				},
+			],
+		},
+		settings: {
+			'@rafaelgomesxyz/i18n-json/ignore-keys': {
+				'**': ['*.description', '*.placeholders'],
+				'**/pt_BR/**': ['serviceScrobble'],
+			},
+		},
+	};
+};
+
+const getJsOverride = () => {
+	return {
+		files: ['**/*.{js,jsx}'],
+		parserOptions: {
+			sourceType: 'module',
+		},
+		extends: [
+			'eslint:recommended',
+			'plugin:react/recommended',
+			'plugin:prettier/recommended', // Displays Prettier errors as ESLint errors. **Make sure this is always the last configuration.**
+		],
+		rules: {
+			quotes: [
+				'error',
+				'single',
+				{
+					avoidEscape: true,
+					allowTemplateLiterals: false,
+				},
+			],
+		},
+	};
+};
+
+const getTsOverride = () => {
+	const tsOverride = getJsOverride();
+	const extendsArr = [
+		'plugin:@typescript-eslint/recommended',
+		'prettier', // Disables TypeScript rules that conflict with Prettier.
+	];
+
+	if (process.env.ESLINT_TYPED === 'true') {
+		tsOverride.parserOptions = {
+			tsconfigRootDir: ROOT_DIR,
+			project: ['./tsconfig.json'],
+		};
+		extendsArr.splice(
+			extendsArr.length - 1,
+			0,
+			'plugin:@typescript-eslint/recommended-requiring-type-checking'
+		);
+	} else {
+		delete tsOverride.parserOptions;
+	}
+
+	tsOverride.files = ['**/*.{ts,tsx}'];
+	tsOverride.plugins = ['prefer-arrow'];
+	tsOverride.extends.splice(tsOverride.extends.length - 1, 0, ...extendsArr);
+	tsOverride.rules['@typescript-eslint/quotes'] = tsOverride.rules.quotes;
+	tsOverride.rules.quotes = 'off';
+	tsOverride.rules['prefer-arrow/prefer-arrow-functions'] = 'error';
+
+	return tsOverride;
+};
+
 module.exports = {
 	env: {
 		browser: true,
@@ -8,72 +89,7 @@ module.exports = {
 		webextensions: true,
 	},
 	rules: {},
-	overrides: [
-		{
-			files: ['**/_locales/**/*.json'],
-			plugins: ['@rafaelgomesxyz/i18n-json'],
-			extends: ['plugin:@rafaelgomesxyz/i18n-json/recommended'],
-			rules: {
-				'@rafaelgomesxyz/i18n-json/identical-keys': [
-					'error',
-					{
-						filePath: path.resolve('./src/_locales/en/messages.json'),
-						checkDuplicateValues: true,
-					},
-				],
-			},
-			settings: {
-				'@rafaelgomesxyz/i18n-json/ignore-keys': {
-					'**': ['*.description', '*.placeholders'],
-					'**/pt_BR/**': ['serviceScrobble'],
-				},
-			},
-		},
-		{
-			files: ['**/*.{js,jsx}'],
-			parserOptions: {
-				sourceType: 'module',
-			},
-			extends: [
-				'eslint:recommended',
-				'plugin:react/recommended',
-				'plugin:prettier/recommended', // Displays Prettier errors as ESLint errors. **Make sure this is always the last configuration.**
-			],
-			rules: {
-				quotes: [
-					'error',
-					'single',
-					{
-						avoidEscape: true,
-						allowTemplateLiterals: false,
-					},
-				],
-			},
-		},
-		{
-			files: ['**/*.{ts,tsx}'],
-			plugins: ['prefer-arrow'],
-			extends: [
-				'eslint:recommended',
-				'plugin:react/recommended',
-				'plugin:@typescript-eslint/recommended',
-				'prettier', // Disables TypeScript rules that conflict with Prettier.
-				'plugin:prettier/recommended', // Displays Prettier errors as ESLint errors. **Make sure this is always the last configuration.**
-			],
-			rules: {
-				quotes: 'off',
-				'@typescript-eslint/quotes': [
-					'error',
-					'single',
-					{
-						avoidEscape: true,
-						allowTemplateLiterals: false,
-					},
-				],
-				'prefer-arrow/prefer-arrow-functions': 'error',
-			},
-		},
-	],
+	overrides: [getTranslationsOverride(), getJsOverride(), getTsOverride()],
 	settings: {
 		react: {
 			version: 'detect',
--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,7 @@ module.exports = {
 	'*.{json,css,html,md,yml,yaml}': 'prettier --write',
 	'*.{js,jsx,ts,tsx}': (fileNames) => [
 		'tsc --noEmit -p ./tsconfig.json',
-		`eslint --fix --quiet -c ./.eslintrc.typed.js --no-eslintrc ${fileNames
+		`cross-env ESLINT_TYPED=true eslint --fix --quiet ${fileNames
 			.map((fileName) => `"${fileName}"`)
 			.join(' ')}`,
 	],
