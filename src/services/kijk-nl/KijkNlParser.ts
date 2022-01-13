0ac0effa7fa86a4957bffd571f7e1b46940b2bf9
ItsYves <info@itsyv.be>
2022-01-13 23:43:26 +0100

    Added Kijk.nl streaming service

---
+++ src/services/kijk-nl/KijkNlParser.ts
@@ -0,0 +1,49 @@
+import { ScrobbleParser } from '@common/ScrobbleParser';
+import { KijkNlApi } from '@/kijk-nl/KijkNlApi';
+import { Item } from '@models/Item';
+
+class _KijkNlParser extends ScrobbleParser {
+	constructor() {
+		super(KijkNlApi, {
+			watchingUrlRegex: /\/films|programmas\/.+\/(.+)/, // https://www.kijk.nl/programmas/steenrijk-straatarm/AaELSZ6dksV
+		});
+	}
+
+	parseItemFromDom() {
+		const serviceId = this.api.id;
+		const titleElement = document.querySelector('title');
+		const id = this.parseItemIdFromUrl();
+		let showTitle: string | null = null;
+		let seasonId: string | null = null;
+		let episodeId: string | null = null;
+
+		// Shows get a title like this (dutch example): "Steenrijk, straatarm - seizoen 2 aflevering 1"
+		const matches = /(.+) - seizoen (\d+) aflevering (\d+)/.exec(titleElement?.textContent ?? '');
+
+		if (matches) {
+			[, showTitle, seasonId, episodeId] = matches;
+		}
+
+		const title = showTitle?.split(' - ')[0] ?? titleElement?.textContent ?? '';
+		const episodeTitle = showTitle?.split(' - ')[1] ?? '';
+		const season = parseInt(seasonId ?? '') || 0;
+		const episode = parseInt(episodeId ?? '') || 0;
+		const type = seasonId ? 'show' : 'movie';
+
+		if (!titleElement) {
+			return null;
+		}
+
+		return new Item({
+			serviceId,
+			id,
+			type,
+			title,
+			episodeTitle,
+			season,
+			episode,
+		});
+	}
+}
+
+export const KijkNlParser = new _KijkNlParser();
