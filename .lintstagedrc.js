77769ab8dda6cb488b87f6f00d84e7e42906ad82
MrMamen <MrMamen@users.noreply.github.com>
2026-02-02 11:59:13 +0100

    remove prettier and include biome.json, and other fixes

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,3 @@
 module.exports = {
-	'*.{css,html,md,yml,yaml}': 'prettier --write',
-	'*.{js,jsx,ts,tsx,json}': ['biome check --write --no-errors-on-unmatched'],
+	'*.{js,jsx,ts,tsx,json,css}': ['biome check --write --no-errors-on-unmatched'],
 };
--- .prettierrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	printWidth: 100,
-	useTabs: true,
-	singleQuote: true,
-};
